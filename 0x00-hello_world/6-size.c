d0ba579a8e7dd761a6f331b9710e131ae23ae067
youssef-frahat <yossiflolo13@gmail.com>
2023-03-19 06:38:34 -0700

    gg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a ;
+    char b ;
+    float c ;
+    double d ;
+        printf( "%d" ,"",sizeof (a));
+        printf( "%d" ,sizeof (b));
+        printf( "%d" ,sizeof (c));
+        printf( "%d" ,sizeof (d));
+    return 0;
+}
